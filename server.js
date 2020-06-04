55510c70d8e2e2289c771086dfa1d905dda1e86f
Rafał Pietrzak <rpietrzak@pixelage.pl>
2020-06-04 10:23:27 +0200

    Add handlebars and nodemon watch

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 const isUser = (req) => {
   return false;
-} 
+}
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
+});
 app.use('/user', (req, res, next) => {
   if(isUser(req)) next();
   else res.send('Go away!');
 });
-
 app.use(express.static(path.join(__dirname, '/public')));
-
 app.use((req, res) => {
   res.status(404);
-  res.show('404.html');
+  res.render('404');
 })
 
 app.listen(8000, () => {
